d01a1bf52cc1acdabdaedbbf6282c4da702969a6
Marcus Pearlman <marcuspearlman@boisestate.edu>
2026-08-19 02:13:42 -0600

    add concat function
    This calls load_cupy every time. This probably needs to be initialized in a class somehow.

--- warpx_helpers/methods.py
+++ warpx_helpers/methods.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from pywarpx import picmi,geometry,amr
-
+from pywarpx.LoadThirdParty import load_cupy
 constants = picmi.constants
 
 def get_grid_cell_sizes():
@@ -50,5 +50,11 @@ def to_cpu_array(arr):
     return np.asarray(arr)
 
 
-
+def concat(list_of_arrays):
+    xp,_ = load_cupy
+    if len(list_of_arrays) == 0:
+        # Return a 1d array of size 0
+        return xp.empty(0)
+    else:
+        return xp.concatenate(list_of_arrays)
         
