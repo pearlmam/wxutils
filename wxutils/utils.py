8c4f8cd95d22258733907cabaa26c870453bce48
Marcus Pearlman <marcuspearlman@boisestate.edu>
2026-08-21 17:44:17 -0600

    add get_rank and delete_diagnostics functions

--- wxutils/utils.py
+++ wxutils/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import shutil
 
 try:
     from mpi4py import MPI as mpi
@@ -11,7 +12,24 @@ def mpi_enabled():
     else:
         comm = mpi.COMM_WORLD
         return comm.Get_size() > 1
-    
+
+def get_rank():
+    if mpi == None:
+        return 0
+    else:
+        comm = mpi.COMM_WORLD
+        return comm.Get_rank()
+        
+def delete_diagnostics():
+    if mpi_enabled():
+        comm = mpi.COMM_WORLD
+        rank = comm.Get_rank()
+    else:
+        rank = 0
+    if rank == 0:
+        shutil.rmtree('./diags',ignore_errors=True)
+        
+        
 def calc_grid_blocking(nx,ny=None,nz=None):
     comm = mpi.COMM_WORLD
     nc = comm.Get_size()
