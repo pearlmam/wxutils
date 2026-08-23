d8e2acb841d58f5f90026f8e8b3e00a9831f0c09
Marcus Pearlman <marcuspearlman@boisestate.edu>
2026-08-22 22:02:27 -0600

    add ops module
    This module is the future plasce for jit optimized functions

---
+++ wxutils/ops/ops.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+'''
+This will possibly be a place to decide which hardware and which optimized functions to use
+'''
+
+# from numba import njit
+
+# @njit(fastmath=True)
+# def g_le(x,x0,x1):
+#     return (x > x0) & (x <= x1)
